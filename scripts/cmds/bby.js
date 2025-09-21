ee7631acd700b3acc42608a089bd380e141be547
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2025-09-21 22:05:06 +0600

    Update bby.js

--- scripts/cmds/bby.js
+++ scripts/cmds/bby.js
@@ -5,7 +5,7 @@ const baseApiUrl = async () => {
 
 module.exports.config = {
     name: "bby",
-    aliases: ["baby", "bbe", "babe", "saif"],
+    aliases: ["mute", "মিউট", "মিউটআনমিউট", "muteunmute"],
     version: "6.9.0",
     author: "dipto",
     countDown: 0,
